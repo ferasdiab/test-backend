4284e63f154ca845fadb4de7fe44682d01f6d1c3
ferasdiab <dfiras96@gmail.com>
2021-11-16 21:51:56 +0300

    add

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+
+const toDos = ["wake up", "eat breakfast", "code"];
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.json("hello from my app");
+});
+
+app.get("/toDos", (req, res) => {
+  res.status(200);
+  res.json(toDos);
+});
+
+
+
+const port = 5000;
+app.listen(port, () => {
+  console.log("server is running");
+});
+
+
+
+
+/*
+const toDos = ["wake up", "eat breakfast", "code"];
+
+app.get("/",(req,res)=>{
+  res.status(200)
+  res.json("hello from my app")
+})
+
+app.get("/toDos",(req,res)=>{
+  res.status(200)
+  res.json(toDos);
+});
+
+
+
+
+*/
